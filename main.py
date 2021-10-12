e35bcf6b694baa1612aa6a8f2f796cec0442e991
Harold Burbano <haroldburbano2002@gmail.com>
2021-10-11 22:11:04 -0300

    Sistema de puntajes terminado

--- main.py
+++ main.py
@@ -1,4 +1,74 @@
 import os
+from random import randint
+
+
+def playerExist(fileLines,playerName) :
+    print("we are in playerexist")
+    for i in fileLines :
+        if i.split(":")[0] ==playerName:
+            return True
+    return False 
+
+
+def getPoints(fileLines,playerName) :
+    print(f"fileline : {fileLines}")
+    for i in fileLines :
+        if i.split(":")[0]==playerName :
+            return int(i.split(":")[1])
+    return -1
+
+def writePoints(playerName,points) :
+
+    content = []
+    with open("./txt-files/players.txt","r") as file :
+        content = list(filter(lambda x : x!="\n" , file.readlines()))
+        print(f"este es el contenido : {content}")
+        file.close()
+
+    with open("./txt-files/players.txt","w+") as file :
+
+        boolean = False #This is for verify if the player points are larger than in the file
+        userExist = playerExist(content,playerName) 
+        print(userExist)
+        if userExist :
+            filePoints = getPoints(content,playerName)
+            print("filePoints = " + str(filePoints))
+            if filePoints < points :
+                boolean = True
+                content.pop(content.index(f"{playerName}:{filePoints}\n"))
+        
+        if(not userExist) or boolean :
+            arr = []
+            flag = 1
+
+            for i in content :
+                if (int(i.split(":")[1]) < points) and flag != 0 :
+                    arr.append(f"{playerName}:{points}\n")
+                    arr.append(i)
+                    flag = 0
+                else :
+                    arr.append(i)
+                print(arr)
+            
+            if flag==1 :
+                arr.append(f"{playerName}:{points}")
+            print(arr)
+            for i in arr :
+                file.write(f"{i}\n")
+
+        else :
+            print(content)
+            for i in content :
+                file.write(f"{i}\n")
+
+        file.close()
+
+def getWord() :
+    word = []
+    with open("./txt-files/words.txt","r") as file :
+        content = file.readlines()
+        word = content[randint(0,len(content)-1)].split(":")
+    return word
 
 def remplazarLetra(array,a) :
     arr = []
@@ -12,7 +82,7 @@ def remplazarLetra(array,a) :
     return arr
 
 
-def imprimirPantalla(playerName,points,lives,wordArray) :
+def imprimirPantalla(playerName,points,lives,wordArray,definition) :
 
     print(
     """
@@ -25,7 +95,8 @@ def imprimirPantalla(playerName,points,lives,wordArray) :
     """)
     print(f"\t{playerName} you have : {points} points\n\n")
     print(f"\tLives = {lives}\n\n")
-    
+    print(f"\tDefinition : {definition}\n\n")
+
     for i in range(len(wordArray)) :
         if i== 0 :
             print(f"\t{wordArray[i]}",end=" ")
@@ -39,26 +110,34 @@ def main():
     playerName = input("Write your nickname : ")
     lives = 1
     points = 0
-    word = "harold"
-    wordArray = list(map(lambda x: "_",word))
+    word = getWord()
+    wordArray = list(map(lambda x: "_",word[0]))
 
-    while(lives) :
+    while(lives!=0) :
 
-        imprimirPantalla(playerName,points,lives,wordArray)
+        imprimirPantalla(playerName,points,lives,wordArray,word[1])
         leter = input("Escribe una palabra : ")
 
-        if leter in word :
+        if leter in word[0] :
             arr = []
-            for i in range(len(word)) :
-                if leter == word[i] :
+            for i in range(len(word[0])) :
+                if leter == word[0][i] :
                     arr.append(leter)
                 else :
                     arr.append(wordArray[i])
             wordArray = arr
+            points +=1
         else :
             lives -=1
         
         os.system("clear")
+
+
+    if(lives==0) :
+        print(f"You lose :( the word guess was : {word[0]}")
+
+    writePoints(playerName,points)
+
 
 if __name__ == "__main__" :
     main()
